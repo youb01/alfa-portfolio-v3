19a53fa3f08bde01dcb1f09f0f0606b096ed31d4
youb01 <lfatmi.ayoub@gmail.com>
2026-03-02 16:22:20 +0100

    Refactor overflow handling in useNavigation and usePreloader hooks for consistency

--- src/hooks/usePreLoader.ts
+++ src/hooks/usePreLoader.ts
@@ -9,11 +9,11 @@ export const usePreloader = () => {
     if (isLoading) {
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = "";
     }
 
     return () => {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = "";
     };
   }, [isLoading]);
 
